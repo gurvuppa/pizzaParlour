2c517fff1ed038b941b7efd94c6256c3caeb0af6
Gurvir Uppal <gurviruppal@icloud.com>
2021-05-03 13:41:03 -0400

    Add project files.

---
+++ pizzaParlour/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace pizzaParlour
+{
+    partial class background
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(background));
+            this.label1 = new System.Windows.Forms.Label();
+            this.topppingLabel = new System.Windows.Forms.Label();
+            this.inputLabel = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.label1.Location = new System.Drawing.Point(-1, -1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(649, 86);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pizza Parlour";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // topppingLabel
+            // 
+            this.topppingLabel.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.topppingLabel.Location = new System.Drawing.Point(67, 134);
+            this.topppingLabel.Name = "topppingLabel";
+            this.topppingLabel.Size = new System.Drawing.Size(287, 33);
+            this.topppingLabel.TabIndex = 1;
+            this.topppingLabel.Text = "Number of Toppings:";
+            // 
+            // inputLabel
+            // 
+            this.inputLabel.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputLabel.Location = new System.Drawing.Point(293, 131);
+            this.inputLabel.Name = "inputLabel";
+            this.inputLabel.Size = new System.Drawing.Size(61, 32);
+            this.inputLabel.TabIndex = 2;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Aqua;
+            this.calculateButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.calculateButton.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateButton.ForeColor = System.Drawing.SystemColors.Window;
+            this.calculateButton.Location = new System.Drawing.Point(260, 193);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(121, 38);
+            this.calculateButton.TabIndex = 3;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.BackColor = System.Drawing.Color.White;
+            this.totalLabel.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalLabel.ForeColor = System.Drawing.Color.Black;
+            this.totalLabel.Location = new System.Drawing.Point(140, 267);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(359, 54);
+            this.totalLabel.TabIndex = 4;
+            this.totalLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // background
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(647, 380);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.inputLabel);
+            this.Controls.Add(this.topppingLabel);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "background";
+            this.Text = "Pizza Parlour";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label topppingLabel;
+        private System.Windows.Forms.TextBox inputLabel;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label totalLabel;
+    }
+}
+
---
+++ pizzaParlour/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pizzaParlour
+{
+    public partial class background : Form
+    {
+        double basePrice = 7.00;
+        double eachTopping = 1.25;
+        double toppingAmount;
+        double totalPrice;
+
+        public background()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                toppingAmount = Convert.ToDouble(inputLabel.Text);
+                totalPrice = basePrice + toppingAmount * eachTopping;
+                totalLabel.Text = $"The price of a pizza with {toppingAmount} is: {totalPrice.ToString("C")}";
+            }
+            catch
+            {
+                totalLabel.Text = "Please insert the amount of toppings as a number";
+            }
+
+        }
+    }
+}
